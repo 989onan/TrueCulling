cfda5cc7dc207e913a5ac201d8715d3ee6a30bdc
989onan <onanchew@gmail.com>
2025-05-19 14:38:41 -0400

    Add project files.

---
+++ TrueCulling/TrueCulling.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+
+namespace TrueCulling
+{
+    public class TrueCulling : ResoniteMod
+    {
+        public override string Author => "989onan";
+        public override string Link => "Nope";
+        public override string Name => "True Culling";
+        public override string Version => "1.0.0";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony($"{Author}.{Name}");
+            harmony.PatchAll();
+        }
+
+        
+    }
+}
